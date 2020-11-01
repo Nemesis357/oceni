8503f1738e91b3a81c85f18c3087850303416460
Nenad <nenadn89@gmail.com>
2020-11-01 19:49:33 +0100

    Added city autocomplete

---
+++ src/js/components/UI/AutoComplete/AutoComplete.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class AutoComplete extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            search: "",
+            cities: []
+        }
+
+    }
+
+    handleSearch = (e) => {
+        if ( e.target.value !== "" ) {
+            axios.post('http://localhost:8000/api/cityAutocomplete', {search: e.target.value})
+                    .then( res => {
+                        this.setState({
+                            cities: res.data
+                        })
+                    })
+                    .catch( res => {
+                        console.log('%c [FAIL] ', 'color:#bada55;', res);
+                    })
+        } else {
+            this.setState({
+                cities: []
+            })
+        }
+
+        this.setState({
+            search: e.target.value
+        })
+    }
+
+    render() {
+        const citiesList = this.state.cities.map(city => <li key={city.id}>{city.name}</li>)
+        return (
+            <div>
+                <input 
+                    type="text" 
+                    value={this.state.search}
+                    onChange={this.handleSearch} 
+                    className="AutoComplete-input"
+                    ref="acInput"
+                    placeholder="Grad"
+                />
+                <ul>
+                    { citiesList }
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default AutoComplete;
--- src/js/screens/Home/Home.js
+++ src/js/screens/Home/Home.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import axios from 'axios';
+import React, { Component } from 'react';
 
-const Home = (props) => {
+import AutoComplete from '../../components/UI/AutoComplete/AutoComplete';
 
-    axios.get('http://localhost:8000/api/getConfig/').then(res => {
-        console.log('%c res [SUCCESS]', 'color:#bada55;', res);
-    }).catch(res => {
-        console.log('%c res [FAILED]', 'color:#bada55;', res);
-    })
+class Home extends Component {
 
-    return <p>Hello from Home component </p>
+    componentDidMount() {
+        // axios.get('http://localhost:8000/api/city/1').then(res => {
+        //     console.log('%c res [SUCCESS]', 'color:#bada55;', res);
+        // }).catch(res => {
+        //     console.log('%c res [FAILED]', 'color:#bada55;', res);
+        // })
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello from Home component </p>
+                <AutoComplete />
+            </div>
+        )
+    }
 }
 
 export default Home;
